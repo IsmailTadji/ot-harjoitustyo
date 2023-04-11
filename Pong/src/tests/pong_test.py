6671a1865d8ac2b92a425ba87acb6b2a75563436
Ismail Tadji <ismail.tadji@gmail.com>
2023-04-11 12:58:43 +0300

    tests

---
+++ Pong/src/tests/pong_test.py
@@ -0,0 +1,18 @@
+import unittest
+import os, sys
+from logic.Pong import Pong
+
+width, height = 450,400 
+
+class PongTest(unittest.TestCase):
+
+    def setUp(self):
+        self.pong = Pong()
+
+    def test_rackets_x_and_y(self):
+        pos1 = (self.pong.racket_p1.x, self.pong.racket_p1.y)
+        self.assertEqual(pos1, (10,200))
+        pos2 = (self.pong.racket_p2.x, self.pong.racket_p2.y)
+        self.assertEqual(pos2, (430,200))
+
+    
